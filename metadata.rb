d4c2a4d1475dc45cb3bb6d7a6ecfbff83ca88ef9
Michael Nikitochkin <miry.sof@gmail.com>
2014-05-11 12:28:42 +0300

    Init cookbook

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'custom_packages'
+maintainer       'Michael Nikitochkin'
+maintainer_email 'michael.a.nikitochkin#@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures packages'
+long_description 'Installs/Configures packages'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: custom_packages
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
